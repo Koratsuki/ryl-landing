cd157f1ac24508c5f0713b7ec2353d7212022085
Leslie León <koratsuki.nijuusan@gmail.com>
2026-02-01 02:39:26 -0500

    feat: updated site structure and versions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,24 @@
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   compressHTML: true,
-});
+  devToolbar: {
+    enabled: false,
+  },
 
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'es'],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
